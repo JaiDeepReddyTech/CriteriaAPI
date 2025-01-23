d136c6902fd476776376d6a22f40367d361ce030
JaiDeepReddyTech <jaideepreddy197@gmail.com>
2025-01-23 13:21:25 +0530

    first commit

---
+++ src/test/java/com/demo/CriteriaAPI/CriteriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.CriteriaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriteriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
